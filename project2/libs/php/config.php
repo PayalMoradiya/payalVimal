6c8369d498d282e5e50d1cf145dc87a3ba907a6a
Payal <pdkabariya@gmail.com>
2021-07-02 21:39:23 +0100

    1

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,15 +1,17 @@
 
 <?php
 
-//	$cd_host = "127.0.0.1";
-//	$cd_port = 3308;
-//	$cd_socket = "";
-//	$cd_user = "root"; // user name
-//	$cd_password = "Payalvimal93"; // password
-//	$cd_dbname = "companydirectory"; // database name
+	$cd_host = "127.0.0.1";
+	$cd_port = 3308;
+	$cd_socket = "";
+	$cd_user = "root"; // user name
+	$cd_password = "Payalvimal93"; // password
+	$cd_dbname = "companydirectory"; // database name
 
 //	$cd_host = "192.168.0.100";
 //	$cd_port = 3306;
 //	$cd_user = "payalvi2_root"; // user name
 //	$cd_password = "Payalvimal93"; // password
-//	$cd_dbname = "payalvi2_companydirectory"; // database
+//	$cd_dbname = "payalvi2_companydirectory"; // database name
+
+?>   
