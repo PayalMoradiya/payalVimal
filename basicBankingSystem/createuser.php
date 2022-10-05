760c95a22a31e659ce35df1df7e398a9bba36c83
PayalMoradiya <75732958+PayalMoradiya@users.noreply.github.com>
2022-10-05 15:59:51 +0100

    Updated Query

--- basicBankingSystem/createuser.php
+++ basicBankingSystem/createuser.php
@@ -17,7 +17,7 @@
     $name=$_POST['name'];
     $email=$_POST['email'];
     $balance=$_POST['balance'];
-    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+    $sql="INSERT into users(name,email,balance) values('$name','$email','$balance')";
     $result=mysqli_query($conn,$sql);
     if($result){
                echo "<script> alert('Hurray! User created');
