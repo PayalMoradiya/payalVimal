517e562ef7c91116b805fa7dfde5cd8f326037ee
PayalMoradiya <75732958+PayalMoradiya@users.noreply.github.com>
2022-10-19 12:24:46 +0100

    test

--- myBlogs/js/comment.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){ 
-    $('.reply').on('click', function(){
-        var commentId = $(this).attr("id");
-        $('#commentId').val(commentId);
-        $('#comment').focus();
-    });
-});
--- myblogs/includes/config.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$db = mysqli_connect("localhost", "root", "", "myblogs") or die("Database is not connected!");
-?>
